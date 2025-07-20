c9b0cbe8e15bb8583a59d7d521a229a3014fb07c
Emre Can Kurt <can.kurt764@gmail.com>
2025-07-20 15:56:06 +0300

    first commit

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,23 @@
+dmx.config({
+  "productPage": {
+    "cartStore": [
+      {
+        "type": "number",
+        "name": "prd_id"
+      },
+      {
+        "type": "text",
+        "name": "prd_name"
+      },
+      {
+        "type": "number",
+        "name": "prd_price"
+      },
+      {
+        "type": "number",
+        "name": "quantity"
+      }
+    ]
+  },
+  "cart": {}
+});
